3db0dceb7dff00ee94e5f64592cbd3c048b205dc
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-16 04:10:49 +0900

    Update 01_select_from_use_alias_limit_distinct_221009.sql

--- 02_데이터_가져오기/01_select_from_use_alias_limit_distinct_221009.sql
+++ 02_데이터_가져오기/01_select_from_use_alias_limit_distinct_221009.sql
@@ -1,24 +1,16 @@
--- Q1. SELECT / FROM) products_df의 products에서 product_name 출력하기
-SELECT product_name
-FROM products_df.products;
-
--- Q2. USE) USE를 사용해 products_df의 products에서 product_name 출력하기
+-- Q1. SELECT & FROM / USE - USE를 사용해 products_df의 products에서 product_name 출력하기
 USE products_df;
 SELECT product_name
 FROM products;
 
--- Q3-1. AS) products_df의 products에서 product_name을 new_product_name로 변경하여 출력하기
+-- Q2-1. AS(Alias) - products_df의 products에서 product_name을 new_product_name으로 컬럼명 임시 변경하여 출력하기
 SELECT product_name AS new_product_name
 FROM products_df.products;
 
--- Q3-2. ALTER TABLE) price를 new_price로 컬럼명 변경하기
+-- Q2-2. ALTER TABLE - price를 new_price로 실제 컬럼명 변경하여 출력하기
 ALTER TABLE products CHANGE COLUMN price new_price INT;
 
--- Q4. LIMIT) products_df의 products에서 product_name을 10개만 출력하기
-SELECT product_name
+-- Q3. LIMIT / DISTINCT - products_df의 products에서 중복을 제외한 product_name을 10개 출력하기
+SELECT DISTINCT product_name
 FROM products_df.products
 LIMIT 10;
-
--- Q5. DISTINCT) products_df의 products에서 product_name을 중복 없이 출력하기
-SELECT DISTINCT product_name
-FROM products_df.products;
