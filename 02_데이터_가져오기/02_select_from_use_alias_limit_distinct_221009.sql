4a503fd2016c464ee6fdff443b94cc232c13c96a
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-15 23:26:29 +0900

    Update 02_select_from_use_alias_limit_distinct_221009.sql

--- 02_데이터_가져오기/02_select_from_use_alias_limit_distinct_221009.sql
+++ 02_데이터_가져오기/02_select_from_use_alias_limit_distinct_221009.sql
@@ -7,11 +7,11 @@ USE products_df;
 SELECT product_name
 FROM products;
 
--- Q3-1. AS) products_df의 products에서 product_name을 'new_product_name'로 변경하여 출력하기
+-- Q3-1. AS) products_df의 products에서 product_name을 new_product_name로 변경하여 출력하기
 SELECT product_name AS new_product_name
 FROM products_df.products;
 
--- Q3-2. ALTER TABLE) price를 'new_price'로 컬럼명 변경하기
+-- Q3-2. ALTER TABLE) price를 new_price로 컬럼명 변경하기
 ALTER TABLE products CHANGE COLUMN price new_price INT;
 
 -- Q4. LIMIT) products_df의 products에서 product_name을 10개만 출력하기
