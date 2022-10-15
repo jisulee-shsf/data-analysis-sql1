2fe921a9643a671f8deb88bf909c2830b95cb92c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-16 05:54:54 +0900

    Update 01_order_by_rank_221016.sql

--- 04_원하는_데이터_만들기/01_order_by_rank_221016.sql
+++ 04_원하는_데이터_만들기/01_order_by_rank_221016.sql
@@ -1,24 +1,24 @@
--- Q1-1. ORDER BY - ASC) products에서 product_name을 price 오름차순으로 출력하기
+-- Q1-1. ORDER BY - products에서 product_name을 price 오름차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price;
 
--- Q1-2. ORDER BY - DESC) products에서 product_name을 price 내림차순으로 출력하기
+-- Q1-2. ORDER BY - products에서 product_name을 price 내림차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price DESC;
 
--- Q1-3. ORDER BY - 정렬 조건 나열) products에서 product_name을 price 내림차순 및 product_id 오름차순으로 출력하기
+-- Q1-3. ORDER BY - products에서 product_name을 price 내림차순 후, product_id 오름차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price DESC, product_id;
 
--- Q1-4. ORDER BY - 정렬 조건 번호 대체) products에서 product_name / price / product_id를 price 내림차순 및 product_id 오름차순으로 출력하기
+-- Q1-4. ORDER BY - products에서 product_name / price / product_id를 price 내림차순 후 product_id 오름차순으로 출력하기
 SELECT product_name, price, product_id
 FROM products
 ORDER BY 2 DESC, 3;
 
--- Q2. RANK / DENSE_RANK / ROW_RANK) student에서 id / name / score를 각 RANK 함수 조건으로 출력하기
+-- Q2. RANK / DENSE_RANK / ROW_RANK - student에서 id / name / score를 각 RANK 함수 조건에 맞추어 출력하기
 SELECT id, name, score
      , RANK() OVER(ORDER BY score DESC) AS rank_list
      , DENSE_RANK() OVER(ORDER BY score DESC) AS dense_rank_list
