b118b0e536b307195e8845f7ad389da8a4bab45e
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-15 23:04:12 +0900

    Update 02_select_from_use_alias_limit_distinct_221009.sql

--- 02_데이터_가져오기/02_select_from_use_alias_limit_distinct_221009.sql
+++ 02_데이터_가져오기/02_select_from_use_alias_limit_distinct_221009.sql
@@ -1,24 +1,24 @@
--- Q1. SELECT / FROM) Products_df 데이터베이스 내에 Products 테이블에서 ProductName 출력하기
-SELECT ProductName
-FROM Products_df.Products;
+-- Q1. SELECT / FROM) products_df의 products에서 product_name 출력하기
+SELECT product_name
+FROM products_df.products;
 
--- Q2. USE) USE를 사용해 Products_df 데이터베이스 내에 Products 테이블에서 ProductName 출력하기
-USE Products_df;
-SELECT ProductName
-FROM Products;
+-- Q2. USE) USE를 사용해 products_df의 products에서 product_name 출력하기
+USE products_df;
+SELECT product_name
+FROM products;
 
--- Q3-1. AS) Products_df 데이터베이스 내에 Products 테이블에서 ProductName 'New_ProductName'으로 출력하기
-SELECT ProductName AS New_ProductName
-FROM Products_df.Products;
+-- Q3-1. AS) products_df의 Products에서 product_name 'new_product_name'로 변경하여 출력하기
+SELECT product_name AS new_product_name
+FROM products_df.products;
 
--- Q3-2. ALTER TABLE) Price를 'New_Price'으로 컬럼명 변경하기
-ALTER TABLE Products CHANGE COLUMN Price New_Price INT;
+-- Q3-2. ALTER TABLE) price를 'new_price'로 컬럼명 변경하기
+ALTER TABLE products CHANGE COLUMN price new_price INT;
 
--- Q4. LIMIT) Products_df 데이터베이스 내에 Products 테이블에서 ProductName을 10개만 출력하기
-SELECT ProductName
-FROM Products_df.Products
+-- Q4. LIMIT) products_df의 products에서 product_name 10개 출력하기
+SELECT product_name
+FROM products_df.products
 LIMIT 10;
 
--- Q5. DISTINCT) Products_df 데이터베이스 내에 Products 테이블에서 ProductName을 중복 없이 출력하기
-SELECT DISTINCT ProductName
-FROM Products_df.Products;
+-- Q5. DISTINCT) products_df의 products에서 product_name을 중복 없이 출력하기
+SELECT DISTINCT product_name
+FROM products_df.products;
