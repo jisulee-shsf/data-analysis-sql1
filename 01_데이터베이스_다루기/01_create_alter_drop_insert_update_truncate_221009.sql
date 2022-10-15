87ff1267fe46d9907662355603b9e0aa9f28178c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-16 01:15:57 +0900

    Update and rename 01_create_alter_drop_insert_truncate_221009.sql to 01_create_alter_drop_insert_update_truncate_221009.sql

--- 01_데이터베이스_다루기/01_create_alter_drop_insert_update_truncate_221009.sql
+++ 01_데이터베이스_다루기/01_create_alter_drop_insert_update_truncate_221009.sql
@@ -23,25 +23,33 @@ ALTER TABLE student MODIFY COLUMN score FLOAT;
 -- Q6. 기존 컬럼 이름 변경 및 타입 변경하기
 ALTER TABLE student CHANGE COLUMN ids id FLOAT;
 
--- Q7. 컬럼 삭제하기
+-- 07. 기존 컬럼 타입만 변경하기
+ALTER TABLE student MODIFY major varchar(30);
+
+-- Q8. 컬럼 삭제하기
 ALTER TABLE student DROP COLUMN score;
 
--- Q8. 테이블 내에 단수 데이터 삽입하기
+-- Q9. 테이블 내에 단수 데이터 삽입하기
 INSERT INTO student(id, name, major)
 VALUES(1, 'A', 'english');
 
--- Q9. 테이블 내에 복수 데이터 삽입하기
+-- Q10. 테이블 내에 복수 데이터 삽입하기
 INSERT INTO student(id, name, major)
 VALUES(2, 'B', 'math'),
       (3, 'C', 'music');
       
--- Q10. 데이터베이스 & 테이블 정보 확인하기
+-- Q11. 테이블 내에 특정 데이터 수정하기
+UPDATE student
+SET major = 'mathematics'
+WHERE id = 2;
+
+-- Q12. 데이터베이스 & 테이블 정보 확인하기
 SHOW DATABASES;
 SHOW TABLES;
 
--- Q11. 테이블 값만 삭제하기
+-- Q13. 테이블 값만 삭제하기
 TRUNCATE TABLE student;
 
--- Q12. 데이터베이스 & 테이블 삭제하기
+-- Q14. 데이터베이스 & 테이블 삭제하기
 DROP DATABASE school; 
 DROP TABLE student;
