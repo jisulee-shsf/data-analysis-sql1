d155adf47873887c6dbebc54efb7457157dfbd97
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:35:24 +0900

    Update 01_if_ifnull_case_221018.sql

--- 06_규칙_만들기/01_if_ifnull_case_221018.sql
+++ 06_규칙_만들기/01_if_ifnull_case_221018.sql
@@ -1,18 +1,18 @@
 -- 1-1. IF
-SELECT name
-     , IF(score >= 70, 'level_a', 'level_b') AS score_if
-FROM student;
+SELECT id
+     , IF(score1 >= 80, 'level_a', 'level_b') AS class_list
+FROM student_list;
 
 -- 1-2. IFNULL
 SELECT id
-     , IFNULL(remarks, '없음') AS score_ifnull
-FROM student;
+     , IFNULL(remarks, '없음') AS remarks_list
+FROM student_list;
 
 -- 2. CASE
 SELECT id, 
 CASE
-     WHEN score >= 30 AND score <= 100 THEN 'class_a'
-     WHEN score < 30 THEN 'class_b'
+     WHEN score1 >= 80 THEN 'class_a'
+     WHEN score1 < 80 THEN 'class_b'
      ELSE 'class_c'
-END AS class_list
+     END AS class_list
 FROM student_list;
