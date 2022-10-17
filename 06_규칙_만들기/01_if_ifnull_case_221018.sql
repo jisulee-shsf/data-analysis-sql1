86669677cc41161ca3028155932013d8e3ec507f
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:32:12 +0900

    Update 01_if_ifnull_case_221018.sql

--- 06_규칙_만들기/01_if_ifnull_case_221018.sql
+++ 06_규칙_만들기/01_if_ifnull_case_221018.sql
@@ -1,24 +1,18 @@
--- Q1-1. IF
--- SELECT 절 내에 단수의 조건을 생성해 결과값을 새로운 컬럼으로 반환하는 IF 실습
+-- 1-1. IF
 SELECT name
      , IF(score >= 70, 'level_a', 'level_b') AS score_if
 FROM student;
 
--- Q1-2. IFNULL
--- SELECT 절 내에서 입력 데이터가 NULL인 경우, 결과값을 새로운 컬럼으로 반환하는 IFNULL 실습
+-- 1-2. IFNULL
 SELECT id
      , IFNULL(remarks, '없음') AS score_ifnull
 FROM student;
 
--- Q2. CASE
--- SELECT 절 내에 복수의 조건을 생성해 결과값을 새로운 컬럼으로 반환하는 CASE 실습
--- CASE의 경우, 복수의 조건을 만들지만 결과값 컬럼은 1개만 생성
--- ELSE 문장 생략 시, 조건식의 거짓에 해당하는 값에 NULL 값 반환
--- 'CASE WHEN + 조건식~ / CASE column_name WHEN 조건값~'의 2가지 형식 존재
-SELECT price, 
+-- 2. CASE
+SELECT id, 
 CASE
-     WHEN score >= 30 AND score <= 100 THEN 'score_a'
-     WHEN score < 30 THEN 'score_b'
-     ELSE 'out_of_range'
-END AS score_case
-FROM student;
+     WHEN score >= 30 AND score <= 100 THEN 'class_a'
+     WHEN score < 30 THEN 'class_b'
+     ELSE 'class_c'
+END AS class_list
+FROM student_list;
