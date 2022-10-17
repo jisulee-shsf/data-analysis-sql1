b11990341d905e162850803e452bd916a1e1b47c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:28:13 +0900

    Update 01_order_by_rank_221016.sql

--- 04_원하는_데이터_만들기/01_order_by_rank_221016.sql
+++ 04_원하는_데이터_만들기/01_order_by_rank_221016.sql
@@ -1,26 +1,26 @@
--- Q1-1. ORDER BY - products에서 product_name을 price 오름차순으로 출력하기
+-- 1-1. ORDER BY - products에서 product_name을 price 오름차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price;
 
--- Q1-2. ORDER BY - products에서 product_name을 price 내림차순으로 출력하기
+-- 1-2. ORDER BY - products에서 product_name을 price 내림차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price DESC;
 
--- Q1-3. ORDER BY - products에서 product_name을 price 내림차순 후, product_id 오름차순으로 출력하기
+-- 1-3. ORDER BY - products에서 product_name을 price 내림차순 후, product_id 오름차순으로 출력하기
 SELECT product_name
 FROM products
 ORDER BY price DESC, product_id;
 
--- Q1-4. ORDER BY - products에서 product_name / price / product_id를 price 내림차순 후 product_id 오름차순으로 출력하기
+-- 1-4. ORDER BY - products에서 product_name / price / product_id를 price 내림차순 후 product_id 오름차순으로 출력하기
 SELECT product_name
      , price
      , product_id
 FROM products
 ORDER BY 2 DESC, 3;
 
--- Q2. RANK / DENSE_RANK / ROW_RANK - student에서 id / name / score를 각 RANK 함수 조건에 맞추어 출력하기
+-- 2. RANK / DENSE_RANK / ROW_RANK - student에서 id / name / score를 각 RANK 함수 조건에 맞추어 출력하기
 SELECT id
      , name
      , score
