042bd4bd55b6f531d7be79b22b28ea2e14e96cae
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:29:32 +0900

    Update 01_group_by_having_group_functions_221017.sql

--- 05_데이터_그룹화하기/01_group_by_having_group_functions_221017.sql
+++ 05_데이터_그룹화하기/01_group_by_having_group_functions_221017.sql
@@ -1,20 +1,20 @@
--- Q1. GROUP BY
+-- 1. GROUP BY
 SELECT category_id
 FROM products
 GROUP BY category_id;
 
--- Q2. Group fuctions - COUNT / SUM / AVG / MIN & MAX
+-- 2. Group fuctions - COUNT / SUM / AVG / MIN & MAX
 SELECT category_id, COUNT(*), COUNT(1), SUM(price), AVG(price), MIN(price), MAX(price)
 FROM products
 GROUP BY category_id;
 
--- Q3. HAVING
+-- 3. HAVING
 SELECT category_id, SUM(price)
 FROM products
 GROUP BY category_id
 HAVING SUM(price) > 100;
 
--- Q4. Query execution order
+-- 4. Query execution order
 /* Products 테이블에서 SupplierID가 10 이하인 데이터를 CategoryID 컬럼 기준으로 그룹화하고, 
 그룹화된 데이터의 Price 최댓값이 30을 초과하는 CategoryID / Price 평균값 / Price 최댓값을 Price 평균값 내림차순으로 출력하기 */ 
 SELECT category_id, AVG(price), MAX(price)
