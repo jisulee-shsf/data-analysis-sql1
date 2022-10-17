01d8645e52a3cf888ce892129823a938fb8ce3e3
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:29:03 +0900

    Update 03_numeric_functions_221017.sql

--- 04_원하는_데이터_만들기/03_numeric_functions_221017.sql
+++ 04_원하는_데이터_만들기/03_numeric_functions_221017.sql
@@ -1,24 +1,24 @@
--- Q1. Numeric functions - ABS
+-- 1. Numeric functions - ABS
 SELECT ABS(score_float)
 FROM student;
 
--- Q2. Numeric functions - CELING / FLOOR
+-- 2. Numeric functions - CELING / FLOOR
 SELECT CELING(score_float)
      , FLOOR(score_float)
 FROM student;
 
--- Q3. Numeric functions - ROUND / TRUNCATE
+-- 3. Numeric functions - ROUND / TRUNCATE
 SELECT ROUND(score_float)
      , TRUNCATE(score_float)
 FROM student;
 
--- Q4. Numeric functions - POWER
+-- 4. Numeric functions - POWER
 SELECT id
      , name
      , POWER(score, 3)
 FROM student;
 
--- Q5. Numeric functions - MOD
+-- 5. Numeric functions - MOD
 SELECT id
      , name
      , MOD(score, 2)
