34e9954f32368e5b40ad197ad00b96e5279de0bc
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 05:26:34 +0900

    Update 01_create_alter_drop_insert_update_truncate_221009.sql

--- 01_데이터베이스_다루기/01_create_alter_drop_insert_update_truncate_221009.sql
+++ 01_데이터베이스_다루기/01_create_alter_drop_insert_update_truncate_221009.sql
@@ -1,11 +1,11 @@
--- Q1. 데이터베이스 & 테이블이 존재할 경우, 삭제하기
+-- 1. 데이터베이스 & 테이블이 존재할 경우, 삭제하기
 DROP DATABASE IF EXISTS school; 
 DROP TABLE IF EXISTS student;
 
--- Q2. 데이터베이스 생성하기
+-- 2. 데이터베이스 생성하기
 CREATE DATABASE school;
 
--- Q3. 데이터베이스 사용 선언 및 테이블 생성하기
+-- 3. 데이터베이스 사용 선언 및 테이블 생성하기
 USE school;
 CREATE TABLE test_student(
        ids INT,
@@ -13,40 +13,40 @@ CREATE TABLE test_student(
        major VARCHAR(10)
 );
 
--- Q4. 테이블명 변경
+-- 4. 테이블명 변경
 ALTER TABLE test_student RENAME student;
 
--- Q5. 컬럼 추가
+-- 5. 컬럼 추가
 ALTER TABLE student ADD COLUMN score INT;
 ALTER TABLE student MODIFY COLUMN score FLOAT;
 
--- Q6. 기존 컬럼명 변경하기
+-- 6. 기존 컬럼명 변경하기
 ALTER TABLE student CHANGE COLUMN ids id FLOAT;
 
--- Q7. 컬럼 삭제하기
+-- 7. 컬럼 삭제하기
 ALTER TABLE student DROP COLUMN score;
 
--- Q8. 단수 데이터 삽입하기
+-- 8. 단수 데이터 삽입하기
 INSERT INTO student(id, name, major)
 VALUES(1, 'A', 'english');
 
--- Q9. 복수 데이터 삽입하기
+-- 9. 복수 데이터 삽입하기
 INSERT INTO student(id, name, major)
 VALUES(2, 'B', 'math'),
       (3, 'C', 'music');
       
--- Q10. 기존 특정 데이터 변경하기
+-- 10. 기존 특정 데이터 변경하기
 UPDATE student
 SET major = 'mathematics'
 WHERE id = 2;
 
--- Q11. 데이터베이스 & 테이블 정보 확인하기
+-- 11. 데이터베이스 & 테이블 정보 확인하기
 SHOW DATABASES;
 SHOW TABLES;
 
--- Q12. 데이터 값 삭제하기
+-- 12. 데이터 값 삭제하기
 TRUNCATE TABLE student;
 
--- Q13. 데이터베이스 / 테이블 삭제하기
+-- 13. 데이터베이스 / 테이블 삭제하기
 DROP DATABASE school; 
 DROP TABLE student;
