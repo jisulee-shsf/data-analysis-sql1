ee96dce75b1ca262ef89e474f26ad24ede6b2403
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-18 01:59:57 +0900

    Update 03_numeric_functions_221017.sql

--- 04_원하는_데이터_만들기/03_numeric_functions_221017.sql
+++ 04_원하는_데이터_만들기/03_numeric_functions_221017.sql
@@ -1,19 +1,25 @@
--- Q1. 숫자형 데이터 함수 - ABS
+-- Q1. Numeric functions - ABS
 SELECT ABS(score_float)
 FROM student;
 
--- Q2. 숫자형 데이터 함수 - CELING / FLOOR
-SELECT CELING(score_float), FLOOR(score_float)
+-- Q2. Numeric functions - CELING / FLOOR
+SELECT CELING(score_float)
+     , FLOOR(score_float)
 FROM student;
 
--- Q3. 숫자형 데이터 함수 - ROUND / TRUNCATE
-SELECT ROUND(score_float), TRUNCATE(score_float)
+-- Q3. Numeric functions - ROUND / TRUNCATE
+SELECT ROUND(score_float)
+     , TRUNCATE(score_float)
 FROM student;
 
--- Q4. 숫자형 데이터 함수 - POWER
-SELECT id, name, POWER(score, 3)
+-- Q4. Numeric functions - POWER
+SELECT id
+     , name
+     , POWER(score, 3)
 FROM student;
 
--- Q5. 숫자형 데이터 함수 - MOD
-SELECT id, name, MOD(score, 2)
+-- Q5. Numeric functions - MOD
+SELECT id
+     , name
+     , MOD(score, 2)
 FROM student;
