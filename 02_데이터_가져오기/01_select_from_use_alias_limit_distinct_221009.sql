67d74c62cde11c37d91346770febdae9f3f81f6f
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-20 07:50:16 +0900

    Update 01_select_from_use_alias_limit_distinct_221009.sql

--- 02_데이터_가져오기/01_select_from_use_alias_limit_distinct_221009.sql
+++ 02_데이터_가져오기/01_select_from_use_alias_limit_distinct_221009.sql
@@ -1,16 +1,20 @@
--- 1. SELECT & FROM / USE - USE를 사용해 products_df의 products에서 product_name 출력하기
+-- 1. SELECT & FROM / USE
+-- Q. products_df의 products_table에서 product_name 출력하기
 USE products_df;
 SELECT product_name
-FROM products;
+FROM products_table;
 
--- 2-1. AS(Alias) - products_df의 products에서 product_name을 new_product_name으로 컬럼명 임시 변경하여 출력하기
+-- 2. AS(Alias)
+-- Q. products_df의 products_table에서 product_name 컬럼명을 new_product_name로 임시 변경하여 출력하기
 SELECT product_name AS new_product_name
-FROM products_df.products;
+FROM products_df.products_table;
 
--- 2-2. ALTER TABLE - price를 new_price로 실제 컬럼명 변경하여 출력하기
-ALTER TABLE products CHANGE COLUMN price new_price INT;
+-- 3. ALTER TABLE
+-- Q. products_table 내에 price 컬럼명을 new_price로 변경하기
+ALTER TABLE products_table CHANGE COLUMN price new_price INT;
 
--- 3. LIMIT / DISTINCT - products_df의 products에서 중복을 제외한 product_name을 10개 출력하기
+-- 4. LIMIT / DISTINCT
+-- Q. products_df의 products_table에서 중복을 제외한 product_name을 10개 출력하기
 SELECT DISTINCT product_name
-FROM products_df.products
+FROM products_df.products_table
 LIMIT 10;
