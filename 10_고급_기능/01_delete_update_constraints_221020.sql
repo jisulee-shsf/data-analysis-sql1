5a200d0d1242207589ce6f58c3ff3951ac05796c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 04:15:44 +0900

    Update 01_delete_update_constraints_221020.sql

--- 10_고급_기능/01_delete_update_constraints_221020.sql
+++ 10_고급_기능/01_delete_update_constraints_221020.sql
@@ -1,10 +1,10 @@
 -- 1-1. DELETE
--- Q. student_list2에서 score1이 60 이상일 때 삭제하기
+# student_list2에서 score1이 60 이상일 때 삭제하기
 DELETE FROM student_list2
 WHERE score1 >= 60;
 
 -- 1-2. UPDATE
--- Q. student_list2에서 id가 4인 score1을 30으로 변경하기
+# student_list2에서 id가 4인 score1을 30으로 변경하기
 UPDATE student_list2
 SET score1 = 30
 WHERE id = 4;
