4f1e43352735469123fe6431ef5f1f3feefbf154
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 04:13:02 +0900

    Update 01_if_ifnull_case_221018.sql

--- 06_규칙_만들기/01_if_ifnull_case_221018.sql
+++ 06_규칙_만들기/01_if_ifnull_case_221018.sql
@@ -1,17 +1,17 @@
 -- 1-1. IF
--- Q. student_list에서 score1이 80 이상인 경우 'level_a'를, 아닌 경우 'level_b'를 기입한 class_list 컬럼과 id 출력하기
+# student_list에서 score1이 80 이상인 경우 'level_a'를, 아닌 경우 'level_b'를 기입한 class_list 컬럼과 id 출력하기
 SELECT id
      , IF(score1 >= 80, 'level_a', 'level_b') AS class_list
 FROM student_list;
 
 -- 1-2. IFNULL
--- Q. student_list에서 remarks가 공란인 경우 '없음'을 기입한 remarks_list 컬럼과 id 출력하기
+# student_list에서 remarks가 공란인 경우 '없음'을 기입한 remarks_list 컬럼과 id 출력하기
 SELECT id
      , IFNULL(remarks, '없음') AS remarks_list
 FROM student_list;
 
 -- 2. CASE
--- Q. student_list에서 score1이 80 이상인 경우 'class_a'를, 80 미만인 경우 'class_b'를, 이외 경우는 'class_c'를 기입한 class_list 컬럼과 id 출력하기
+# student_list에서 score1이 80 이상인 경우 'class_a'를, 80 미만인 경우 'class_b'를, 이외 경우는 'class_c'를 기입한 class_list 컬럼과 id 출력하기
 SELECT id,
 CASE
     WHEN score1 >= 80 THEN 'class_a'
