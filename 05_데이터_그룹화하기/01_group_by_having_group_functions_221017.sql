245e57433708db41b215a7b2d20960298cfcf7b3
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-20 18:21:50 +0900

    Update 01_group_by_having_group_functions_221017.sql

--- 05_데이터_그룹화하기/01_group_by_having_group_functions_221017.sql
+++ 05_데이터_그룹화하기/01_group_by_having_group_functions_221017.sql
@@ -1,22 +1,25 @@
 -- 1. GROUP BY
+-- Q. products에서 category_id 기준으로 그룹화해 category_id 출력하기
 SELECT category_id
 FROM products
 GROUP BY category_id;
 
--- 2. Group fuctions - COUNT / SUM / AVG / MIN & MAX
-SELECT category_id, COUNT(*), COUNT(1), SUM(price), AVG(price), MIN(price), MAX(price)
-FROM products
-GROUP BY category_id;
-
--- 3. HAVING
+-- 2. HAVING
+-- Q. products에서 category_id 기준으로 그룹화 된 데이터 중, price 총합이 100 초과인 category_id와 price 총합 출력하기
 SELECT category_id, SUM(price)
 FROM products
 GROUP BY category_id
 HAVING SUM(price) > 100;
 
+-- 3. Group fuctions - COUNT / SUM / AVG / MIN & MAX
+-- Q. products에서 category_id 기준으로 그룹화해 category_id의 데이터 수 / price 총합 / price 평균 / price 최솟값 & 최댓값 출력하기
+SELECT category_id, COUNT(*), COUNT(1), SUM(price), AVG(price), MIN(price), MAX(price)
+FROM products
+GROUP BY category_id;
+
 -- 4. Query execution order
-/* Products 테이블에서 SupplierID가 10 이하인 데이터를 CategoryID 컬럼 기준으로 그룹화하고, 
-그룹화된 데이터의 Price 최댓값이 30을 초과하는 CategoryID / Price 평균값 / Price 최댓값을 Price 평균값 내림차순으로 출력하기 */ 
+/* Q. products에서 supplier_id가 10 이하인 데이터를 category_id 기준으로 그룹화하고, 
+그룹화된 데이터의 price 최댓값이 30을 초과하는 category_id / price 평균값 / price 최댓값을 price 평균값 내림차순으로 출력하기 */
 SELECT category_id, AVG(price), MAX(price)
 FROM products
 WHERE supplier_id <= 10
