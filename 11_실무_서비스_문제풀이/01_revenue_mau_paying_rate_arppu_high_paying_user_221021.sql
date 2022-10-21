8c306d2f768544f68a2f008686197d9bcd57a759
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 12:04:11 +0900

    Update 01_revenue_mau_paying_rate_arppu_high_paying_user_221021.sql

--- 11_실무_서비스_문제풀이/01_revenue_mau_paying_rate_arppu_high_paying_user_221021.sql
+++ 11_실무_서비스_문제풀이/01_revenue_mau_paying_rate_arppu_high_paying_user_221021.sql
@@ -1,5 +1,5 @@
 -- 1. Revenue 
-# 2021년 10월 Revenue를 'AS oct_revenue'명으로 출력하기
+# 2021년 10월 Revenue를 oct_revenue명으로 출력하기
 SELECT SUM(price) AS oct_revenue
 FROM purchase_list
 WHERE purchased_at LIKE '2021-10%';
@@ -10,7 +10,8 @@ WHERE purchased_at >= '2021-10-01'
   AND purchased_at < '2021-11-01';
   
 -- 2. MAU(Monthly Active Users)
-# 2021년 10월 MAU 출력하기 / 1회라도 방문한 기록이 있을 경우 active user로 판단
+# 2021년 10월 MAU 출력하기
+# 1회라도 방문한 기록이 있을 경우 active user로 판단
 SELECT COUNT(DISTINCT customer_id)
 FROM visit_list
 WHERE visited_at >= '2021-10-01'
@@ -18,7 +19,7 @@ WHERE visited_at >= '2021-10-01'
   
 -- 3. Paying rate
 # 2021년 10월 MAU의 Paying rate 출력하기
-# Paying rate: 구매 고객 비율(실 구매 고객 수/ 전체 활성 고객 수)
+# Paying rate - 구매 고객 비율(실 구매 고객 수/ 전체 활성 고객 수)
 SELECT COUNT(DISTINCT customer_id)
 FROM purchase_list
 WHERE purchased_at >= '2021-10-01'
@@ -35,19 +36,19 @@ SELECT ROUND(11174 / 16414 * 100, 2) AS oct_paying_rate;
 # 2021년 10월 구매 고객의 월 평균 구매액 출력하기
 SELECT AVG(revenue)
 FROM (SELECT customer_id
-	       , SUM(price) AS revenue
-	  FROM purchase_list
+	   , SUM(price) AS revenue
+      FROM purchase_list
       WHERE purchased_at >= '2021-10-01'
         AND purchased_at < '2021-11-01'
-	  GROUP BY customer_id) AS inline_view_subquery;
+      GROUP BY customer_id) AS inline_view_subquery;
 
 -- 5. 고과금 고객
 # 2021년 10월 가장 많이 구매한 고객 중, 상위 11번째부터 15번째 고객의 고객 id / 총 지출 비용 / 순위 출력하기
 SELECT customer_id, revenue, RANK() OVER (ORDER BY revenue DESC) AS revenue_rank
 FROM (SELECT customer_id
-		   , SUM(price) AS revenue
-	  FROM purchase_list
-      WHERE purchased_at >= '2020-07-01'
-        AND purchased_at < '2020-08-01'
-	  GROUP BY customer_id) AS inline_view_subquery
+	   , SUM(price) AS revenue
+      FROM purchase_list
+      WHERE purchased_at >= '2021-10-01'
+        AND purchased_at < '2021-11-01'
+      GROUP BY customer_id) AS inline_view_subquery
 LIMIT 5 OFFSET 10;
