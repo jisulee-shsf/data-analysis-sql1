6ad87db20e8ed6dd81875fb1da68472d6a959b72
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-22 08:08:18 +0900

    Update 01_dau_wau_221022.sql

--- 12_날짜_및_시간별_실무_분석_문제풀이/01_dau_wau_221022.sql
+++ 12_날짜_및_시간별_실무_분석_문제풀이/01_dau_wau_221022.sql
@@ -6,8 +6,7 @@ SELECT DATE_FORMAT('2021-10-01', '%Y-%M-%D');
 SELECT DATE_FORMAT('2021-10-01', '%y-%m-%d %p');
 SELECT DATE_FORMAT(NOW(), '%Y-%m');
 
--- 1. DAU
--- 2021년 10월의 평균 DAU를 구하기
+-- 1. DAU - 2021년 10월의 평균 DAU를 구하기
 # interval 확인
 SELECT visited_at 
      , DATE_FORMAT(visited_at, '%Y-%m-%d %r') AS date_at
@@ -32,8 +31,7 @@ FROM (SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%d') AS date_at
         AND visited_at < '2021-10-01'
       GROUP BY 1) AS inline_view_subquery;
 
--- 2. WAU
--- 2021년 10월의 평균 WAU를 구하기
+-- 2. WAU - 2021년 10월의 평균 WAU를 구하기
 # 한 주가 완전한 7일인 경우의 WAU 출력
 SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%U') AS date_at
      , COUNT(DISTINCT customer_id) AS active_users
