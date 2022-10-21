23a1b14a206796d0343764db8c24cae53ba39d9b
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-22 03:17:37 +0900

    Update 01_dau_wau_221022.sql

--- 12_날짜_및_시간별_실무_분석_문제풀이/01_dau_wau_221022.sql
+++ 12_날짜_및_시간별_실무_분석_문제풀이/01_dau_wau_221022.sql
@@ -6,37 +6,37 @@ SELECT DATE_FORMAT('2021-10-01', '%Y-%M-%D');
 SELECT DATE_FORMAT('2021-10-01', '%y-%m-%d %p');
 SELECT DATE_FORMAT(NOW(), '%Y-%m');
 
--- 1. DAU(Daily Active Users)
--- 2021년 10월의 평균 DAU를 구하고, active user 수가 증가하는 추세인지 확인하기
+-- 1. DAU
+-- 2021년 10월의 평균 DAU를 구하기
 # interval 확인
 SELECT visited_at 
      , DATE_FORMAT(visited_at, '%Y-%m-%d %r') AS date_at
 FROM visit_list
 WHERE visited_at >= '2021-10-01' 
-AND visited_at < '2021-10-01';
+  AND visited_at < '2021-10-01';
 
 # interval 제외 후, DAU 출력
 SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%d') AS date_at
      , COUNT(DISTINCT customer_id) 
 FROM visit_list
 WHERE visited_at >= '2021-10-01' 
-AND visited_at < '2021-10-01'
+  AND visited_at < '2021-10-01'
 GROUP BY 1;
 
 # 평균 DAU 출력
 SELECT AVG(active_users)
 FROM (SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%d') AS date_at
            , COUNT(DISTINCT customer_id) AS active_users
-	  FROM visit_list
+      FROM visit_list
       WHERE visited_at >= '2021-10-01'
         AND visited_at < '2021-10-01'
       GROUP BY 1) AS inline_view_subquery;
 
--- 2. WAU(Weekly Active Users)
--- 2021년 10월의 평균 WAU를 구하고, active user 수가 증가하는 추세인지 확인하기
-# 한 주가 완전한 7일인 경우의 WAU만 출력
+-- 2. WAU
+-- 2021년 10월의 평균 WAU를 구하기
+# 한 주가 완전한 7일인 경우의 WAU 출력
 SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%U') AS date_at
-	 , COUNT(DISTINCT customer_id) AS active_users
+     , COUNT(DISTINCT customer_id) AS active_users
 FROM visit_list
 WHERE visited_at >= '2021-10-03'
   AND visited_at < '2021-10-31'
@@ -48,5 +48,5 @@ FROM (SELECT DATE_FORMAT(visited_at - INTERVAL 9 HOUR, '%Y-%m-%U') AS date_at
            , COUNT(DISTINCT customer_id) AS active_users
       FROM visit_list
       WHERE visited_at >= '2021-10-03'
-		AND visited_at < '2021-10-31'
+	AND visited_at < '2021-10-31'
       GROUP BY 1) AS inline_view_subquery;
